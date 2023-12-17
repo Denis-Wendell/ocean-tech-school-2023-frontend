e81268f880101526cde94e45192694054b0c5c1e
Bowie <do7281@gmail.com>
2023-12-17 15:35:27 -0400

    ultimas modificações

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className="header">
       <a href="/" className="logo-link">
-        <img
-          src="https://oceanbrasil.com/assets/logo.svg"
-          alt="Logo do Ocean"
+        <img 
+          src="https://i.etsystatic.com/24961489/r/il/a46e82/2636191477/il_340x270.2636191477_8khh.jpg" style={{width:"4rem"}}
+          alt="Imagem do Rick and Morty"
         />
       </a>
 
